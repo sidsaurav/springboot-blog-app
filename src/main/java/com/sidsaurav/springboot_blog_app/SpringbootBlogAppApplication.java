7cb72e6f2f729c31d977876adf3b248255da2587
Siddharth Saurav <sid.saurav11@gmail.com>
2024-06-14 14:50:34 +0530

    init commit

---
+++ src/main/java/com/sidsaurav/springboot_blog_app/SpringbootBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.sidsaurav.springboot_blog_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogAppApplication.class, args);
+	}
+
+}
